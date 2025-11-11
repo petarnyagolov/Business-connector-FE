6a89825b727728ff4f5c99f983ec1f159483eea2
petarnyagolov <petyrnyagolov@gmail.com>
2025-11-11 19:57:54 +0200

    Fix dev environments

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,7 @@
 ﻿export const environment = {
   production: false,
-  apiUrl: 'http://localhost:8081', 
-  websocketUrl: 'ws://localhost:8081/ws'
+  // apiUrl: 'http://localhost:8081', 
+  // websocketUrl: 'ws://localhost:8081/ws'
+    apiUrl: 'https://api.xdealhub.com',
+  websocketUrl: 'wss://api.xdealhub.com/ws'
 };
